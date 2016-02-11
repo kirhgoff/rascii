07dd75c4993413d9eac29d149b3d7e421437b2fc
Kirill Lastovirya <kirill.lastovirya@gmail.com>
2016-02-11 10:02:38 +0300

    nearly finished

--- src/main.rs
+++ src/main.rs
@@ -13,25 +13,40 @@ fn main() {
     println!("{:?}", img.color());
 
     let (width, height) = img.dimensions();
-    let asciiWidth = 30;
-    let asciiHeight = asciiWidth * height / width;
-    let mut sums:Vec<Vec<i32>> = Vec::new();
+    let ascii_width = 30;
+    let ascii_height = ascii_width * height / width;
+    let cell_width = width / ascii_width;
+    let cell_height = height / ascii_height;
 
-    for i in 0..asciiWidth {
-      sums.push(Vec::new());
-      for j in 0..asciiHeight {
-        let mut row = sums.get(i as usize).unwrap();
-        row.push(0);
+    let mut sums:Vec<i32> = Vec::new();
+
+    //Iterate over every cell
+    for row in 0..ascii_width {
+      for column in 0..ascii_height {
+        let mut current:i32 = 0;
+        for x in (row*cell_width)..((row+1)*cell_width) {
+          for y in (column*cell_height)..((column + 1)*cell_height) {
+            let pixel = img.get_pixel(x, y);
+            current += pixel.data[0] as i32;
+          }
+        }
+        sums.push(current);
       }
     }
 
-
-    for x in 0..width {
-      for y in 0..height {
-        let pixel = img.get_pixel(x, y);
-        println!("{} {} {}", pixel.data[0], pixel.data[1], pixel.data[2]);
+    //Normalize
+    let mut min = std::i32::MAX;
+    let mut max = std::i32::MIN;
+    for x in sums {
+      if x < min {
+        min = x;
+      }
+      if x > max {
+        max = x;   
       }
     }
+
+    println!("Done!");
 }
 
 
